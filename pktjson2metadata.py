0a8ec1401b8fd8155323b3b7f8490a03a9ac134b
Brett Kuskie <fullaxx@gmail.com>
2023-03-11 13:54:58 -0700

    Ditch frame.number and frame.cap_len

--- pktjson2metadata.py
+++ pktjson2metadata.py
@@ -79,11 +79,11 @@ def processPackets(packets):
 		packetID = x.hexdigest()
 		x.reset()
 
-		ndjson={}
+		ndjson = {}
 		ndjson['packetID'] = packetID
 		ndjson['captureTime'] = frame['frame.time_epoch']
-		ndjson['frameNumber'] = frame['frame.number']
-		ndjson['frameCapLen'] = frame['frame.cap_len']
+		#ndjson['frameNumber'] = frame['frame.number']
+		#ndjson['frameCapLen'] = frame['frame.cap_len']
 		ndjson['frameProtocols'] = frame['frame.protocols']
 		if int(frame['frame.encap_type']) == 1:
 			dissectEthernet()
